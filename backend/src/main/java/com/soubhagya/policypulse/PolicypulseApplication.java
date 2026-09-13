cdad30ce79267f60709175596557fdbeada06f09
soubhagya-kumar-behera <soubhagyabehera074@gmail.com>
2026-09-13 19:59:33 +0530

    chore: initialize Policy Impact Engine backend

---
+++ backend/src/main/java/com/soubhagya/policypulse/PolicypulseApplication.java
@@ -0,0 +1,13 @@
+package com.soubhagya.policypulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicypulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicypulseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/soubhagya/policypulse/PolicypulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soubhagya.policypulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicypulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
